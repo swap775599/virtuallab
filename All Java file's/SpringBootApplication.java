742a018d1dc6c2064c24458f368028559be743b8
Swapnil Raibole <118529628+swap775599@users.noreply.github.com>
2024-09-14 18:53:36 +0530

    Create SpringBootApplication.java

---
+++ All Java file's/SpringBootApplication.java
@@ -0,0 +1,12 @@
+package your.package;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplication.class, args);
+    }
+}
